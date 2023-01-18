6a7f24a65e30ab2d2d10837fe7ba17ef219b4f82
valery.sotnik <cmvaltime@gmail.com>
2023-01-18 16:47:45 +0100

    01_components: input and output

--- 1-components/cc-inputs-outputs/src/app/app.component.ts
+++ 1-components/cc-inputs-outputs/src/app/app.component.ts
@@ -6,4 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  notificationsCount = 20
+
+  updateNotificationsCount(count: number) {
+    this.notificationsCount = count;
+    }
 }
