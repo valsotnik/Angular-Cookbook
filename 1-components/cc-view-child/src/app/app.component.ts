30964340d2e56d6863ed663451c9fe46cf10cedd
valery.sotnik <cmvaltime@gmail.com>
2023-01-18 18:53:16 +0100

    01_components: access with @ViewChild decorator

--- 1-components/cc-view-child/src/app/app.component.ts
+++ 1-components/cc-view-child/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { GalleryComponent } from './components/gallery/gallery.component';
 
 @Component({
   selector: 'app-root',
@@ -7,12 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cc-view-child';
+  @ViewChild(GalleryComponent) gallery;
 
   addNewPicture() {
     console.log('added new picture');
+    this.gallery.pictures.unshift(this.gallery.generateImage());
   }
 
   removeFirstPicture() {
     console.log('removed first picture');
+    this.gallery.pictures.shift()
   }
 }
