514651181144cc199b34eff37271a9dc900f5bf6
valery.sotnik <cmvaltime@gmail.com>
2023-01-18 17:48:16 +0100

    01_components: intercept input with getters and setters

--- 1-components/cc-setters/src/app/components/vc-logs/vc-logs.component.ts
+++ 1-components/cc-setters/src/app/components/vc-logs/vc-logs.component.ts
@@ -6,10 +6,28 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./vc-logs.component.scss']
 })
 export class VcLogsComponent implements OnInit {
-  @Input() vName;
+  _vName: string;
+
+  @Input()
+    get vName() {
+      return this._vName
+    };
+    set vName(name: string) {
+      if (!name) return;
+
+      (!this._vName) ?
+        this.logs.push(`initial version is ${name.trim()}`) :
+        this.logs.push(`version changed to ${name.trim()}`)
+
+      this._vName = name
+    }
+
+  logs: string[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
 }
+
