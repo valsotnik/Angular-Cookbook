5ebad2e9c31490213d72758a49b9a9a8365446fa
valery.sotnik <cmvaltime@gmail.com>
2023-01-19 10:49:16 +0100

    02_directives: configuration of types in tsconfig for templates

--- 2-directives/enhanced-template-type-checking/src/app/app.component.ts
+++ 2-directives/enhanced-template-type-checking/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HighlightColor } from './directives/highlight.directive';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   searchText = '';
+  hColor: HighlightColor = HighlightColor.LightCoral;
 }
