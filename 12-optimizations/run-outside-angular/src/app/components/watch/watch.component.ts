16fd0c674f0b7544d45ce03c9762159743659da0
valery.sotnik <cmvaltime@gmail.com>
2023-01-24 12:27:14 +0100

    12_optimizations: running async events outside Angular with runOutsideAngular

--- 12-optimizations/run-outside-angular/src/app/components/watch/watch.component.ts
+++ 12-optimizations/run-outside-angular/src/app/components/watch/watch.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, Input, NgZone, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-watch',
@@ -20,7 +20,7 @@ export class WatchComponent implements OnInit {
   xVelocity;
   yVelocity;
   intervalTimer;
-  constructor() {
+  constructor(private zone: NgZone) {
     this.xVelocity = this.minSpeed;
     this.yVelocity = this.minSpeed;
   }
@@ -35,9 +35,11 @@ export class WatchComponent implements OnInit {
       window['appLogs'] = {};
     }
     window['appLogs']['watch'] = 0;
-    this.intervalTimer = setInterval(() => {
-      this.animate();
-    }, 30);
+    this.zone.runOutsideAngular(() => {
+      this.intervalTimer = setInterval(() => {
+        this.animate();
+      }, 30);
+    })
   }
 
   calcSpeed() {
